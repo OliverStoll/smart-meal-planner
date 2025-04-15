f699302ea1a638edeed814deedff85e59ad5fd2d
OliverStoll <oliverstoll.berlin@gmail.com>
2025-04-15 20:55:48 +0200

    Refactoring, better message names and improvement of messaging

--- src/meals.py
+++ src/meals.py
@@ -1,18 +1,16 @@
 import pandas as pd
 import ast
 import os
+from typing import Literal
 
 from utils.logger import create_logger
-from settings_handler import UserSettings
+from settings import UserSettings
 
 
 class HfMealManager:
     """ Handles recipe data, generating shopping lists and PDF paths for recipes."""
 
     log = create_logger('HF-Messager')
-    data_path = 'data/recipes/cleaned_data_v2.csv'
-    ingredients_df_path = 'data/recipes/ingredients_v2.csv'
-    pdf_path = 'data/recipes/pdfs_v2_mobile'
     orig_portions = 2
     home_ingredients = [
         'milder Chili-Mix', 'Gewürzmischung', 'zwiebel', 'schalotte', 'knoblauch',
@@ -20,17 +18,56 @@ class HfMealManager:
         'wasser', 'Madras Curry', 'Madras-Curry', 'Schwarzkümmel'
     ]
     category_order = ['Obst', 'Gemüse', 'Gewürze', 'Brot', 'Fleisch', 'Haltbares', 'Milchprodukte', 'Verschiedenes']
+    quantity_replace_map = {
+        '½': '0.5',
+        '¼': '0.25',
+        '¾': '0.75',
+        '⅓': '0.333',
+        '⅔': '0.667',
+        '⅕': '0.2',
+        '⅛': '0.125',
+        '⅜': '0.375',
+        '⅝': '0.625',
+        '⅞': '0.875'
+    }
 
-    def __init__(self):
-        self.recipe_df = pd.read_csv(self.data_path)
-        self.ingredients_df = pd.read_csv(self.ingredients_df_path)
+    def __init__(
+            self,
+            data_path='data/recipes/cleaned_data_v2.csv',
+            ingredients_df_path='data/recipes/ingredients_v2.csv',
+            pdf_paths='data/recipes/pdfs_v2_mobile'
+    ):
+        self.recipe_df = pd.read_csv(data_path)
+        self.ingredients_df = pd.read_csv(ingredients_df_path)
+        self.pdf_path = pdf_paths
 
     def get_recipe_ingredients_pdfs(
-            self, num_recipes, num_portions=2, meal_type='alle', max_duration=90, min_calories=0
-    ):
-        recipes = self.get_recipes_filtered_by_user_settings(num_recipes, meal_type, max_duration, min_calories)
-        ingredient_shopping_list = self.get_ingredients_shopping_list(recipes, num_portions)
-        recipes_pdf_paths = self.get_pdf_paths_from_recipes(recipes, num_portions)
+            self,
+            num_recipes: int,
+            user_settings: UserSettings | None = None
+    ) -> tuple[str, list[str]]:
+        """
+        Generated the shopping list text and the list of PDF paths for the recipes.
+
+        Args:
+            num_recipes (int): The number of recipes to include.
+            user_settings (UserSettings | None): The user settings for filtering recipes.
+
+        Returns:
+            A tuple containing the shopping list text and a list of PDF paths.
+        """
+        recipes = self.get_recipes_filtered_by_user_settings(
+            num_recipes=num_recipes,
+            user_settings=user_settings
+        )
+        ingredient_shopping_list = self.get_ingredients_shopping_list(
+            recipes_df=recipes,
+            num_portions=user_settings.portions
+        )
+        recipes_pdf_paths = self.get_pdf_paths_from_recipes(
+            recipes=recipes,
+            num_portions=user_settings.portions
+        )
         return ingredient_shopping_list, recipes_pdf_paths
 
     def get_recipes_filtered_by_user_settings(self, num_recipes: int, user_settings: UserSettings) -> pd.DataFrame:
@@ -56,8 +93,8 @@ class HfMealManager:
 
         return recipes_df
 
-    def _filter_recipes_by_meal_type(self, recipes_df: pd.DataFrame, meal_type: str) -> pd.DataFrame:
-        tags = None
+    @staticmethod
+    def _filter_recipes_by_meal_type(recipes_df: pd.DataFrame, meal_type: str) -> pd.DataFrame:
         if meal_type == 'vegetarisch':
             tags = ['vegetarisch', 'vegan']
             recipes_df = recipes_df[recipes_df['tags'].apply(lambda x: any(tag in x for tag in tags))]
@@ -73,7 +110,21 @@ class HfMealManager:
         pdf_title = meal_name.replace(':', '').replace('!', '').replace('&', 'und')
         return pdf_title
 
-    def get_pdf_paths_from_recipes(self, recipes: pd.DataFrame, num_portions: int) -> list[str]:
+    def get_pdf_paths_from_recipes(
+            self,
+            recipes: pd.DataFrame,
+            num_portions: int
+    ) -> list[str]:
+        """
+        Returns the paths to the PDF files for the given recipes.
+
+        Args:
+            recipes (pd.DataFrame): DataFrame containing the recipes.
+            num_portions (int): Number of portions to adjust the PDF paths.
+
+        Returns:
+            list[str]: List of paths to the PDF files.
+        """
         titles = recipes['title'].tolist()
         self.log.info(f"Recipes: {titles}")
         pdf_dir = self.pdf_path
@@ -92,29 +143,54 @@ class HfMealManager:
             recipes_df: pd.DataFrame,
             num_portions: int,
             filter_home_ingredients: bool = True,
-            sorting: str = 'category'
+            sorting: Literal['category', 'amount'] = 'category'
     ) -> str:
+        """
+        Generates a combined shopping list from the recipes DataFrame, taking into account the number of portions.
+
+        Args:
+            recipes_df (pd.DataFrame): DataFrame containing the recipes.
+            num_portions (int): Number of portions to adjust the quantities.
+            filter_home_ingredients (bool): Whether to filter out home ingredients.
+            sorting (str): Sorting method for the ingredients list ('category' or 'amount').
+        """
         ingredients_df = self._get_ingredient_data(recipes_df, filter_home_ingredients)
-        ingredients_df = pd.merge(ingredients_df, self.ingredients_df[['name', 'category']],
-                                  on='name', how='left')
-        ingredients_df['category'] = pd.Categorical(
-            values=ingredients_df['category'],
-            categories=self.category_order,
-            ordered=True
-        )
+        ingredients_df = self._sort_ingredients_data(ingredients_df, sorting)
+
+        ingredients_df['unit'] = ingredients_df['unit'].replace('Stück', '')
+        quantity_factor = float(num_portions / self.orig_portions)
+        ingredients_df['quantity'] = ingredients_df['quantity'] * quantity_factor
+        ingredients_df['quantity'] = ingredients_df['quantity'].apply(lambda x: int(x) if x == int(x) else x)
+        ingredients_list_str = self._generate_ingredients_shopping_list_text(ingredients_df)
+        return ingredients_list_str
+
+    def _sort_ingredients_data(
+            self,
+            ingredients_df: pd.DataFrame,
+            sorting: Literal['category', 'amount']
+    ) -> pd.DataFrame:
+        """
+        Sorts the ingredients DataFrame based on the specified sorting method, either by their categories or amounts.
+
+        Args:
+            ingredients_df (pd.DataFrame): DataFrame containing the ingredients.
+            sorting (str): Sorting method for the ingredients list ('category' or 'amount').
+
+        Returns:
+            Sorted DataFrame of ingredients.
+        """
         if sorting == 'category':
             ingredients_df = ingredients_df.sort_values(['category', 'name'])
         elif sorting == 'amount':
             ingredients_df['is_stueck'] = ingredients_df['unit'] == 'Stück'
-            ingredients_df = ingredients_df.sort_values(by=['is_stueck', 'quantity'],
-                                                        ascending=False)
+            ingredients_df = ingredients_df.sort_values(by=['is_stueck', 'quantity'], ascending=False)
             ingredients_df.drop(columns='is_stueck', inplace=True)
-        factor = float(num_portions / self.orig_portions)
-        ingredients_df['quantity'] = ingredients_df['quantity'] * factor
-        ingredients_df['quantity'] = ingredients_df['quantity'].apply(
-            lambda x: int(x) if x == int(x) else x)
+
+        return ingredients_df
+
+    @staticmethod
+    def _generate_ingredients_shopping_list_text(ingredients_df: pd.DataFrame):
         max_quantity = ingredients_df['quantity'].max()
-        ingredients_df['unit'] = ingredients_df['unit'].replace('Stück', '')
         quantity_digits = len(str(int(max_quantity)))
         ingredients_list_str = ""
         for idx, row in ingredients_df.iterrows():
@@ -128,24 +204,35 @@ class HfMealManager:
     def _get_ingredient_data(self, recipes: pd.DataFrame, filter_home_ingredients) -> pd.DataFrame:
         ingredients = self._collect_ingredients(recipes)
         ingredients_df = pd.DataFrame(ingredients)
-        ingredients_df['quantity'] = ingredients_df['quantity'].replace({
-            '½': '0.5',
-            '¼': '0.25',
-            '¾': '0.75',
-            '⅓': '0.333',
-            '⅔': '0.667',
-            '⅕': '0.2',
-            '⅛': '0.125',
-            '⅜': '0.375',
-            '⅝': '0.625',
-            '⅞': '0.875'
-        })
-        final_ingredients = self._sum_up_duplicate_ingredients(ingredients_df)
-        if filter_home_ingredients:
-            final_ingredients = self._filter_ingredients(final_ingredients)
-        return final_ingredients
+        ingredients_df['quantity'] = ingredients_df['quantity'].replace(self.quantity_replace_map)
 
-    def _collect_ingredients(self, recipes: pd.DataFrame) -> list:
+        unique_ingredients = self._sum_up_duplicate_ingredients(ingredients_df)
+
+        if filter_home_ingredients:
+            unique_ingredients = self._filter_ingredients(unique_ingredients, self.home_ingredients)
+
+        unique_ingredients = self._clean_ingredient_data(unique_ingredients)
+
+        return unique_ingredients
+
+    def _clean_ingredient_data(self, ingredients_df: pd.DataFrame) -> pd.DataFrame:
+        ingredients_df['quantity'] = ingredients_df['quantity'].replace(self.quantity_replace_map)
+
+        ingredients_df = pd.merge(
+            left=ingredients_df,
+            right=self.ingredients_df[['name', 'category']],
+            on='name',
+            how='left'
+        )
+        ingredients_df['category'] = pd.Categorical(
+            values=ingredients_df['category'],
+            categories=self.category_order,
+            ordered=True
+        )
+        return ingredients_df
+
+    @staticmethod
+    def _collect_ingredients(recipes: pd.DataFrame) -> list:
         ingredients_series = recipes['ingredients'].tolist()
         all_ingredients = []
         for ingredient_list_str in ingredients_series:
@@ -154,15 +241,16 @@ class HfMealManager:
                 all_ingredients.append(ingredient)
         return all_ingredients
 
-    def _sum_up_duplicate_ingredients(self, ingredients_df: pd.DataFrame) -> pd.DataFrame:
+    @staticmethod
+    def _sum_up_duplicate_ingredients(ingredients_df: pd.DataFrame) -> pd.DataFrame:
         ingredients_df['quantity'] = ingredients_df['quantity'].astype(float)
         ingredients_group = ingredients_df.groupby(['name', 'unit'])
         ingredients_df = ingredients_group.agg({'quantity': 'sum'})
         ingredients_df.reset_index(inplace=True)
         return ingredients_df
 
-    def _filter_ingredients(self, ingredients_df: pd.DataFrame) -> pd.DataFrame:
-        pattern = '|'.join(self.home_ingredients)
+    def _filter_ingredients(self, ingredients_df: pd.DataFrame, ingredients_to_filter: list[str]) -> pd.DataFrame:
+        pattern = '|'.join(ingredients_to_filter)
         ingredients_df = ingredients_df[~ingredients_df['name'].str.contains(pattern, case=False)]
         return ingredients_df
 
