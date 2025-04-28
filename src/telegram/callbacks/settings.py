6a7862c6d70b6d2ea27152b0f4397d9b790a351f
OliverStoll <oliverstoll.berlin@gmail.com>
2025-04-28 16:21:50 +0200

    Refactoring

--- src/telegram/callbacks/settings.py
+++ src/telegram/callbacks/settings.py
@@ -1,11 +1,12 @@
 import json
 from dataclasses import dataclass
 from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton as InlineButton
-from telebot import types
+from telebot import types, TeleBot
 
 from common_utils.apis.firebase import FirebaseClient
 from common_utils.config import secret
 
+# from src.telegram.recipes import RecipeManager
 
 
 @dataclass
@@ -29,9 +30,9 @@ class UserSettings:
 
 
 class SettingsHandler:
-    callback_delim = '|'
     user_settings_path = 'data/options.json'
     user_settings_ref = 'AppData/Telegram Meal Bot/User Settings'
+    firebase_env = 'FIREBASE_REALTIME_DB_URL'
     settings: dict[str, SettingsType] = {
         'portions': SettingsType(
             name='portions',
@@ -76,14 +77,21 @@ class SettingsHandler:
         )
     }
 
-    def __init__(self, bot, meal_manager):
+    def __init__(
+            self,
+            bot: TeleBot,
+            recipe_manager,  #  : RecipeManager,
+            callback_delimiter: str,
+            firebase_client: FirebaseClient | None = None
+    ):
         self.bot = bot
-        self.meal_manager = meal_manager
-        self.firebase_client = FirebaseClient(realtime_db_url=secret('FIREBASE_REALTIME_DB_URL'))
+        self.recipe_manager = recipe_manager
+        self.callback_delim = callback_delimiter
+        if firebase_client is None:
+            self.firebase_client = FirebaseClient(realtime_db_url=secret(self.firebase_env))
 
-    def handle_settings_callback(self, setting_name: str, message: types.Message):
+    def get_setting_options_menu(self, setting_name: str):
         setting_data = self.settings[setting_name]
-
         keyboard = InlineKeyboardMarkup()
         keyboard_buttons = []
         for setting_option in setting_data.options:
@@ -94,45 +102,45 @@ class SettingsHandler:
             keyboard_buttons.append(button)
         keyboard.row(*keyboard_buttons)
 
-        self.bot.edit_message_text(
-            chat_id=message.chat.id,
-            message_id=message.message_id,
-            text=setting_data.query_message,
-            reply_markup=keyboard
-        )
+        return {
+            'text': setting_data.query_message,
+            'reply_markup': keyboard
+        }
 
-    def handle_user_setting_callback(self, call_data: str, message: types.Message):
+    def handle_user_setting_callback(self, call_data: str, message: types.Message, chat_id: int):
         """
         Handles the callback of the user setting selection. Sets the user setting and sends a confirmation message.
 
         Args:
             call_data (str): The data from the callback query.
             message (types.Message): The original message object used to edit the message and access user data.
+            chat_id (int): The chat ID of the user.
         """
+        # chat_id = message.chat.id
         setting_name, setting_option = call_data.split(self.callback_delim)
-        chat_id = message.chat.id
-        setting = self.settings[setting_name]
         setting_option = self._try_convert_str_to_int(setting_option)
         self.set_user_setting(chat_id=chat_id, setting_name=setting_name, setting_option=setting_option)
 
-        response = self.get_option_confirmation_message(setting=setting, option=setting_option, chat_id=chat_id)
+        setting_type = self.settings[setting_name]
+        response = self.get_option_confirmation_message(setting_type=setting_type, option=setting_option, chat_id=chat_id)
         self.bot.edit_message_text(chat_id=chat_id, message_id=message.message_id, text=response)
 
-    def get_option_confirmation_message(self, setting: SettingsType, option: str | int, chat_id: int | None = None):
+    def get_option_confirmation_message(self, setting_type: SettingsType, option: str | int, chat_id: int | None = None):
         """ 
         Returns a confirmation message for the selected setting option
         
         Args:
-            setting (SettingsType): The setting type.
+            setting_type (SettingsType): The setting type.
             option (str | int): The selected option value.
             chat_id (int | None): The chat ID (optional).
         """
-        if setting.option_labels and option in setting.option_labels:
-            option = setting.option_labels[option]
-        response = setting.confirmation_message.format(value=option)
+        if setting_type.option_labels and option in setting_type.option_labels:
+            option = setting_type.option_labels[option]
+        response = setting_type.confirmation_message.format(value=option)
 
-        if setting.is_filter:
-            num_meal_options = self.get_num_of_options(chat_id=chat_id)
+        if setting_type.is_filter:
+            user_settings = self.get_user_settings(chat_id=chat_id)
+            num_meal_options = self.get_num_of_options(user_settings=user_settings)
             response += f"\n\nEs gibt insgesamt {num_meal_options} passende Gerichte für deine Einstellungen."
         return response
 
@@ -158,22 +166,8 @@ class SettingsHandler:
         user_settings = UserSettings(**user_settings_data)
         return user_settings
 
-    def get_num_of_options(self, chat_id):
-        options_file = json.load(open(self.user_settings_path, 'r'))
-        meal_type = options_file['meal_type'].get(str(chat_id), None)
-        max_duration = options_file['max_duration'].get(str(chat_id), 999)
-        cal_min = options_file['cal_min'].get(str(chat_id), 0)
-        portions = options_file['portions'].get(str(chat_id), 2)
-        user_settings = UserSettings(
-            portions=portions,
-            meal_type=meal_type,
-            max_duration=max_duration,
-            cal_min=cal_min
-        )
-        recipes_df = self.meal_manager.get_recipes_filtered_by_user_settings(
-            num_recipes=999999,
-            user_settings=user_settings
-        )
+    def get_num_of_options(self, user_settings):
+        recipes_df = self.recipe_manager.get_recipes_filtered_by_user_settings(user_settings=user_settings)
         num_options = len(recipes_df)
         return num_options
 
