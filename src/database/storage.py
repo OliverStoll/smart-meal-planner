2381f84251c428fbd36adae72cc5e761580b9b78
OliverStoll <oliverstoll.berlin@gmail.com>
2026-03-13 16:50:16 +0100

    Refactoring

--- src/database/storage.py
+++ src/database/storage.py
@@ -7,12 +7,15 @@ import boto3
 
 load_dotenv()
 
-s3 = boto3.client(
-    "s3",
-    endpoint_url=os.environ["STORAGE_ENDPOINT"],
-    aws_access_key_id=os.environ["STORAGE_KEY_ID"],
-    aws_secret_access_key=os.environ["STORAGE_APPLICATION_KEY"],
-)
+try:
+    s3 = boto3.client(
+        "s3",
+        endpoint_url=os.environ["STORAGE_ENDPOINT"],
+        aws_access_key_id=os.environ["STORAGE_KEY_ID"],
+        aws_secret_access_key=os.environ["STORAGE_APPLICATION_KEY"],
+    )
+except Exception:
+    s3 = None
 
 
 def upload_file(path_or_obj: str | BytesIO, reference: str):
