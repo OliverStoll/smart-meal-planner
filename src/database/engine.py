cc936979476013f73b734ebe4fc6524a43650bad
OliverStoll <oliverstoll.berlin@gmail.com>
2026-03-13 16:25:10 +0100

    Refactoring

--- src/database/engine.py
+++ src/database/engine.py
@@ -1,13 +1,20 @@
 from dotenv import load_dotenv
 from sqlalchemy import create_engine
 import os
+from common_utils.logger import create_logger
 
 from config.settings import PROJECT_NAME
 
 load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
-engine = create_engine(DATABASE_URL, pool_pre_ping=True)
+log = create_logger("Database")
+
+try:
+    engine = create_engine(DATABASE_URL, pool_pre_ping=True)
+except Exception:
+    log.error("DB Engine was not initialized")
+    engine = None
 
 
 def table(table_name: str):
