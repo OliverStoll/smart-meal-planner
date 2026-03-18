8fca585a0ff562d1ec40ab751fd07d8edd74b688
OliverStoll <oliverstoll.berlin@gmail.com>
2026-03-18 12:36:30 +0100

    Reduce unittest time

--- tests/supermarkets/test_crawler.py
+++ tests/supermarkets/test_crawler.py
@@ -9,18 +9,27 @@ def supermarket_product_url():
     return "https://www.supermarktcheck.de/product/209354-milram-milch-reis"
 
 
-@pytest.mark.slow
-def test_scrape_all_products():
-    crawler = SupermarketScraper()
-    results = crawler.scrape_all_products(supermarket="Lidl")
-    print(results)
+@pytest.fixture
+def supermarket_name():
+    return "bio-company"
 
 
 @pytest.mark.slow
-def test_get_all_product_links():
+def test_scrape_all_products(supermarket_name):
     crawler = SupermarketScraper()
-    links = crawler.get_all_product_links(supermarket="Lidl")
-    print(links)  # TODO
+    results = crawler.scrape_all_products(supermarket=supermarket_name)
+    assert (
+        len(results) > 0
+    ), f"Expected to find product data for {supermarket_name}, but found none."
+
+
+@pytest.mark.slow
+def test_get_all_product_links(supermarket_name):
+    crawler = SupermarketScraper()
+    links = crawler.get_all_product_links(supermarket=supermarket_name)
+    assert (
+        len(links) > 0
+    ), f"Expected to find product links for {supermarket_name}, but found none."
 
 
 @pytest.mark.slow
@@ -28,7 +37,9 @@ def test_scrape_product(supermarket_product_url):
     driver = create_driver()
     crawler = SupermarketScraper()
     data = crawler.scrape_product(url=supermarket_product_url, driver=driver)
-    print(data)  # TODO
+    assert (
+        len(data) > 0
+    ), f"Expected to find product data for {supermarket_product_url}, but found none."
     driver.close()
 
 
@@ -39,5 +50,7 @@ def test_scrape_product_threaded(supermarket_product_url):
     data = crawler.scrape_products_threaded(
         product_links=[supermarket_product_url], driver=driver, thread_id=1
     )
-    print(data)  # TODO
+    assert (
+        len(data) > 0
+    ), f"Expected to find product data for {supermarket_product_url}, but found none."
     driver.close()
