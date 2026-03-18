cd9787a1bc99601ece174624a8c32ecc80887ec7
OliverStoll <oliverstoll.berlin@gmail.com>
2026-03-18 10:55:01 +0100

    Update CI and unittests

--- src/web/driver.py
+++ src/web/driver.py
@@ -1,6 +1,5 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
 
 
 def create_driver(headless: bool = True):
